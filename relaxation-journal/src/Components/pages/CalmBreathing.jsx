7ac37d019800dd8aa92009d9857e7a9bfab1e2f2
Brandon Espinosa <espinbrandon49@gmail.com>
2023-05-01 22:19:11 -0700

    populate and style calmBreathing, populate and style reducingTheTension

--- relaxation-journal/src/Components/pages/CalmBreathing.jsx
+++ relaxation-journal/src/Components/pages/CalmBreathing.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const CalmBreathing = () => {
   return (
-    <div>CalmBreathing</div>
+    <div className='page calmBreathing'>
+      <h2>Calm Breathing</h2>
+      <p>How can Learning Calm Breathing Help Me?</p>
+
+      <p>Breathing affects the way we feel.  If we are anxious or frightened, we breate faster to fuel our bodies for action.  If we are relaxed, we slow down our breathing.  We can feel more relaxed and less anxious/stressed by slowing down our breathing and focusing on the breath out.</p>
+
+      <p className='instructions'>Calm Breathing Instructions</p>
+      <ol>
+        <li>Take a normal breath in through your nose with your mouth closed.  Hold your breath for 5 seconds.</li>
+        <li>Exhale slowly through your nose with your mouth closed, letting all the air out of your lungs.</li>
+        <li>Pause for a count of 4 and then take the next breath in.</li>
+        <li>Practice this exercise several times a day and at night while you are in bed preparing to sleep.</li>
+      </ol>
+    </div>
   )
 }
 
--- relaxation-journal/src/Components/pages/ReducingTheTension.jsx
+++ relaxation-journal/src/Components/pages/ReducingTheTension.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ReducingTheTension = () => {
   return (
-    <div>ReducingTheTension</div>
+    <div className='reducingTheTension page'>
+    <h2>Reducing the Tension</h2>
+    <p>There are many ways for you to help reduce your levle of tension and physiological arousal.  Each requires some practice or committment on your part, but these techniques and life changes can significently reduce your level of tension</p>
+
+    <ul>
+      <li>Calm Breathing</li>
+      <li>Progressive muscle relaxation</li>
+      <li>Regular exercise</li>
+      <li>Eat healthy foods (low sugar, low fat)</li>
+      <li>Avoid alcohol</li>
+      <li>Avoid caffeine</li>
+      <li>Quit smoking</li>
+    </ul>
+    </div>
   )
 }
 
