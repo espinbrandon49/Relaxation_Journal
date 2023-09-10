dddaeaab5d8678eacdc4a931214f7b35d2648cad
Brandon Espinosa <espinbrandon49@gmail.com>
2023-09-09 19:12:56 -0700

    update calmbreathing page

--- relaxation-journal/src/Components/pages/calmbreathing/CalmBreathing.jsx
+++ relaxation-journal/src/Components/pages/calmbreathing/CalmBreathing.jsx
@@ -1,6 +1,5 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
-// import script from './script.js';
 
 const CalmBreathing = () => {
   const [text, setText] = useState("");
@@ -30,28 +29,37 @@ const CalmBreathing = () => {
       setText('Hold!');
     }, holdTime * 3)
   };
-  
+
   setInterval(breathAnimation, totalTime);
 
   return (
     <div className='page calmBreathing'>
       <h2>Calm Breathing</h2>
       <h5 className='squareBreathing'>Square Breathing</h5>
-      <p className='instructions'>How can Learning Calm Breathing Help Me?</p>
+      <p >
+        Box breathing is a type of meditation that involves breathing in through your nose while visualizing a box with equal sides. The steps are:
+      </p>
+      {/* <p className='instructions'>How can Learning Calm Breathing Help Me?</p>
 
       <p>Box breathing, also known as four-square breathing, is a breathing technique that involves counting to four while breathing in and out. The name comes from the fact that a box has four sides, and the technique consists of four distinct phases. </p>
 
-      <p>You can use box breathing to relieve stress, calm down, and maintain focus. It can also help you wind down for sleep.</p>
+      <p>You can use box breathing to relieve stress, calm down, and maintain focus. It can also help you wind down for sleep.</p> */}
 
-      <p className='instructions'>Calm Breathing Instructions</p>
+      {/* <p className='instructions'>Calm Breathing Instructions</p> */}
       <ol className='single-list'>
         <li>Close your eyes.</li>
         <li>Breathe in through your nose while counting to four.</li>
         <li>Hold your breath for four seconds.</li>
         <li>Exhale slowly for four seconds.</li>
-        <li>Repeat steps 1 through 4 at least three times.</li>
       </ol>
 
+      <p>
+        You can repeat these steps at least three times. You can do box breathing several times a day as needed to calm your nerves and relieve stress.
+      </p>
+
+      <p>
+        Box breathing can help keep people calm and relaxed while also keeping them alert and focused.
+      </p>
       <div className='relaxer'>
         <div className={size} id="relaxer-container">
           <div className="circle"></div>
@@ -64,7 +72,7 @@ const CalmBreathing = () => {
           <div className="gradient-circle"></div>
         </div>
       </div>
-
+      {/* 
       <p>Box breathing is also called “the Navy SEAL breathing technique”. Highly trained military individuals, professional athletes, police officers, and nurses use box breathing to focus before high-pressure situations.</p>
 
       <p className='instructions'>You can also check out this video:</p>
@@ -72,7 +80,7 @@ const CalmBreathing = () => {
       <div className='iframe'>
         <iframe width="400" height="400" src="https://www.youtube.com/embed/ummIQOQFHZA" title="Square Breathing (Box Breathing) - Coping Skills for Kids" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-      <div className='push'></div>
+      <div className='push'></div> */}
     </div>
   )
 }
